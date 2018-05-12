8320660146faa6c83b8cc52efb904b4256713195
Tyler Gould <tg@tylergould.net>
2018-05-12 09:19:32 -0500

    process tests

---
+++ __tests__/lib/process.spec.js
@@ -0,0 +1,10 @@
+const process = require('../../lib/process');
+
+describe('process', function() {
+  test('should run', function() {
+    const mockContents = '<div></div>';
+
+    const result = process(mockContents);
+    expect(typeof result).toEqual('string');
+  });
+});
