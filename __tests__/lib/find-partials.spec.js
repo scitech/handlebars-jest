57c8b12f369714e839376f7d0cc27c414bc91546
tylergould <tylergould@users.noreply.github.com>
2018-05-15 12:27:35 -0500

    Add partials (#4)
    * add partials
    
    * use cache for related assets
    
    * resolve nested partials and helpers

---
+++ __tests__/lib/find-partials.spec.js
@@ -0,0 +1,93 @@
+const mock = require('mock-fs');
+
+const cache = require('../../lib/cache');
+const findPartials = require('../../lib/find-partials');
+
+describe('findPartials', function() {
+  afterEach(function() {
+    mock.restore();
+    cache.flushAll();
+  });
+
+  test('creates a map of partial names to absolute paths', function() {
+    const intendedPath = '/path/to/partials';
+    mock({
+      [intendedPath]: {
+        'firstpartial.hbs': 'module.exports = function() { return "im a partial" }',
+        'secondpartial.hbs':'module.exports = function() { return "im a partial too" }',
+      }
+    });
+
+    const expectedResult = {
+      firstpartial: '/path/to/partials/firstpartial.hbs',
+      secondpartial: '/path/to/partials/secondpartial.hbs'
+    };
+
+    const result = findPartials([intendedPath]);
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('warns about nonexistent paths', function() {
+    console.warn = jest.fn()
+    const intendedPath = '/path/to/partials';
+
+    mock({
+      [intendedPath]: {
+        'firstpartial.hbs': 'module.exports = function() { return "im a partial" }',
+      }
+    });
+
+    const result = findPartials(['/wrong/path']);
+    expect(console.warn.mock.calls.length).toEqual(1);
+    expect(result).toEqual({});
+  });
+
+  test('only finds hbs', function() {
+    const intendedPath = '/path/to/partials';
+    mock({
+      [intendedPath]: {
+        'notpartial.txt': 'something else',
+      }
+    });
+
+    const result = findPartials([intendedPath]);
+    expect(result).toEqual({});
+  });
+
+  test('subsequent calls use cache', function() {
+    const intendedPath = '/path/to/partials';
+    mock({
+      [intendedPath]: {
+        'notpartial.txt': 'something else',
+      }
+    });
+
+    const result = findPartials([intendedPath]);
+    expect(result).toEqual({});
+    const cacheSetMock = jest.fn();
+    cache.set = cacheSetMock;
+    const secondResult = findPartials([intendedPath]);
+    expect(cacheSetMock.mock.calls.length).toEqual(0);
+    expect(secondResult).toEqual(result);
+  });
+
+  test('finds nested partials', function() {
+    const intendedPath = '/path/to/partials';
+    mock({
+      [intendedPath]: {
+        'firstpartial.hbs': 'module.exports = function() { return "im a partial" }',
+        'dir': {
+          'secondpartial.hbs': 'module.exports = function() { return "im a partial too" }'
+        },
+      }
+    });
+
+    const expectedResult = {
+      firstpartial: '/path/to/partials/firstpartial.hbs',
+      'dir/secondpartial': '/path/to/partials/dir/secondpartial.hbs'
+    };
+
+    const result = findPartials([intendedPath]);
+    expect(result).toEqual(expectedResult);
+  });
+});
